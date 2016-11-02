9264910aa9b064753fc20fb49d1a681a3ec24f79
rootanujraval24 <Anujraval24@gmail.com>
2016-11-02 15:23:03 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+/**
+ * Created by anujr on 02-Nov-16.
+ */
+console.log('New node');
+var person ={
+    firstName: "Anuj",
+    lastName: "Raval",
+    age: 20
+};
+console.log(person);
+ function addnumbers(a , b){
+     return a+b;
+}
+console.log("Addition of two numbers is :" + addnumbers(5,5));
+
