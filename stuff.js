08b7fd87c4b139f9e5fd3059d357b8c593f54fe9
TheNightOwl <Anujraval24@gmail.com>
2017-05-24 04:42:06 +0530

    we can write modules using 3 ways

--- stuff.js
+++ stuff.js
@@ -1,3 +1,4 @@
+//method 1
 var count = function(arr){
   return 'There are ' + arr.length + ' elements in this array';
 };
@@ -13,3 +14,35 @@ var pi=3.1442;
 module.exports.count = count;
 module.exports.adder = adder;
 module.exports.pi = pi;
+
+
+//method 2
+module.exports.count = function(arr){
+  return 'There are ' + arr.length + ' elements in this array';
+};
+
+module.exports.adder = function(a,b){
+  return `The sum of the 2 numbers ${a+b}`;
+};
+
+module.exports.pi=3.1442;
+
+
+//method 3
+var count = function(arr){
+  return 'There are ' + arr.length + ' elements in this array';
+};
+
+var adder = function(a,b){
+  return `The sum of the 2 numbers ${a+b}`;
+};
+
+var pi=3.1442;
+
+//we need to define the module in order to use that module outside of its scope
+module.exports = {
+      count: count,
+      adder: adder,
+      pi: pi
+
+};
