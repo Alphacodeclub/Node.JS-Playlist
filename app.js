93aad469ba77b7ea1df41c0c7f5b62c7f82f762b
TheNightOwl <Anujraval24@gmail.com>
2017-05-24 03:48:37 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,19 +2,19 @@ console.log('node.JS');
 //global object - window object
 
 //setTimeout
-/*setTimeout(function(){
+setTimeout(function(){
   console.log('You have succesfully wasted 10 seconds');
-},10000);*/
+},10000);
 
 //setInterval
-/*var time = 0;
+var time = 0;
 setInterval(function(){
   time +=2;
   console.log('You have succesfully wasted ' +time + ' seconds');
-},2000);*/
+},2000);
 
 //stopping timer by adding condtions to it
-/*
+
 var time = 0;
 var timer = setInterval(function(){
   time +=2;
@@ -22,38 +22,40 @@ var timer = setInterval(function(){
   if (time > 5){
     clearInterval(timer);
   }
-},2000);*/
+},2000);
 
-//to get directory where we are working
-//console.log('Directory name ' +__dirname);
+//to get directory where we are workin
+console.log('Directory name ' +__dirname);
 
 //to get file name
-//console.log('file name ' +__filename);
+console.log('file name ' +__filename);
 
 //function expresstion
 //normal function stmt
-/*function sayHi(){
+function sayHi(){
   console.log('Hi user');
 }
-sayHi();*/
+sayHi();
 
-//function expresstion
-/*var sayBye = function(){
+//function expression
+var sayBye = function(){
   console.log('Bye User');
 };
-sayBye();*/
+sayBye();
 
-/*function callFun(fun){
+function callFun(fun){
   fun();
 }
-callFun(sayBye);*/
+callFun(sayBye);
 
 //modules & require() method
-/*var count = require('./count');
+var count = require('./count');
 //if you put this module outside the module scope this won't work
-console.log(count(['Messi','Neymar','Suarez']));//will throw error*/
+console.log(count(['Messi','Neymar','Suarez']));//will throw error
 
-//Module Patterns
+//modules patterns
 var stuff = require('./stuff');
 //if you put this module outside the module scope this won't work
 console.log(stuff.count(['Messi','Neymar','Suarez']));
+console.log(stuff.adder(6,4));
+console.log(stuff.adder(stuff.pi,5));
