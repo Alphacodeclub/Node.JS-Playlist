e97e173f1f23e12c80f796e40e95e6284e2ba0c0
TheNightOwl <Anujraval24@gmail.com>
2017-05-24 05:12:11 +0530

    Commit 2 / Description added

--- event.js
+++ event.js
@@ -1,13 +1,15 @@
 //node event emitter
-var events = require('events');
-var util = require('util');
+var events = require('events');//will call out events
+var util = require('util');//will call out util
 
+//Will take the name as person
 var person = function(name){
   this.name=name;
 };
 
-util.inherits(person,events.EventEmitter);
+util.inherits(person,events.EventEmitter);//using EventEmitter we used/called events
 
+//created some objects for person to emit
 var messi = new person('Leo Messi');
 var dele = new person('Dele Ali');
 var neymar = new person('Neymar Jr');
@@ -15,10 +17,11 @@ var people = [messi,dele,neymar];
 
 people.forEach(function(person){
   person.on('speak',function(msg){
-    console.log(person.name + ' said ' + msg);
+    console.log(person.name + ' said ' + msg);//prints out what person emit using EventEmitter
   });
 });
 
+//person emitted
 messi.emit('speak','Champions of Copa Del Rey & golden boot pichichi');
 dele.emit('speak','We are second in premire league');
 neymar.emit('speak','We are also second in la liga but Champions of Copa Del Rey')
